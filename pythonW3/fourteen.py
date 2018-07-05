8326cc2d1c88aa6b62efcd71b1cb76d0ab460e15
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-07-05 22:07:11 +0200

    final refrence push

---
+++ pythonW3/fourteen.py
@@ -0,0 +1,8 @@
+# Date function 
+
+import datetime
+
+i = datetime.datetime.now()
+print(i)
+
+# also there is a lot of methods you can see at https://www.w3schools.com/python/python_datetime.asp
--- pythonW3/module/thirteen.py
+++ pythonW3/module/thirteen.py
@@ -1,2 +1,10 @@
 # you can choose a short name for module to import JUST BY TYPING "as" keyword
 
+import subthirteen as st
+import platform
+
+i = platform.system()
+print(i)
+
+a = st.dictionary["age"]
+print(a)
