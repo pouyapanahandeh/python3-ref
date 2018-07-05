c61b8679441fd268ac22b7cce1d2d3d182b761b2
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-07-05 02:00:07 +0200

    class in python

--- pythonW3/elevenn.py
+++ pythonW3/elevenn.py
@@ -3,7 +3,31 @@
 class Employee:
 	
 	x = 5
+
+# empOne is object for class Eployee ==> in Java ==> NameOfClass object = NameOfClass();
 	
 empOne = Employee()
 print(empOne.x)
 
+# the __init__() function which is simillar to the Constructor in java
+
+class Honda:
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age
+		
+honda = Honda("pooya", 25)
+
+print(honda.name)
+print(honda.age)
+
+# class with function 
+
+class Mazda:
+	def __init__(self, name, age, email):
+		self.name = name 
+		self.age = age
+		self.email = name+"@company.com"
+
+def user:
+	code
