614392969537637bf4787f61e192fbe8c0b514c0
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-07-28 16:37:51 -0500

    while loop

---
+++ python3-sentdex/four.py
@@ -0,0 +1,10 @@
+# while loop 
+
+condition: int = 1
+while condition <= 10:
+    print(condition)
+    '''blow we just use short version of condition = condition + 1'''
+    condition += 1
+
+# while True:
+#     print('Do it in python')
