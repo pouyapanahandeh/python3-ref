916b019650d046b67ab1c4956d8ca48a56fb637c
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-07-28 14:47:47 -0500

    print function and different type of printing

--- python3-sentdex/one.py
+++ python3-sentdex/one.py
@@ -1,3 +1,25 @@
 #print function in python 3
 
+print('This is an example of print function')
+
+# you can use "" also
+
+print("This is a function too")
+
+# for skip ' we need backSlash like example
+
+print('we are going to \' Airshow \' we will have fun')
+
+# you can concat two string to each other 
+
+print('I am pooya' + ' welcome to my github')
+
+# you  can concat by just puthng , 
+
+print('read the comment','do not move blindly')
+
+# if you wanna have something like print('high'+ 5) we will get the error then the best way is using the built in function
+
+print('High'+str(5)) # the str() is built in function to parse number to string
+
 
