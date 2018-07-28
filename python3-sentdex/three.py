a6f97cfdb4a4438ea9c7936ac0ce5620e5f4ccfa
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-07-28 15:49:42 -0500

    variable

---
+++ python3-sentdex/three.py
@@ -0,0 +1,20 @@
+# define variable in python
+
+varOne = 55
+
+print(varOne)
+
+varTwo = 55 + 45
+
+print(varTwo)
+
+varThree = print('whoa')
+
+# you can asign multiple variable once
+
+x,y = (3,5)
+
+print(x)
+print(y)
+
+# also you can put variable equal function
