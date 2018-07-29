503748d399721faff0f7dcfa2a2854a7dec8ce34
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-07-29 01:39:12 -0500

    function

---
+++ python3-sentdex/seven.py
@@ -0,0 +1,9 @@
+# function in python
+
+def simpleAddition(numOne,numTwo):
+    answer = numOne + numTwo
+    print('number one is:',numOne)
+    print('number two is:',numTwo)
+    print(answer)
+
+simpleAddition(8,16)
