d78baf180554c280e7cf86493538179bdf462b5f
pouyapanahandeh <pooya.panahandeh@hotmail.com>
2018-07-29 01:09:13 -0500

    if,else, else if

---
+++ python3-sentdex/six.py
@@ -0,0 +1,31 @@
+x = 4
+y = 8
+z = 5
+if x<y:
+    print('x is less than y')
+
+
+if z < y > x:
+    print('y is greater than z and greater than x')
+
+# if else 
+
+a = 3
+b = 6
+
+if a<b:
+    print('b is greater than a')
+else:
+    print('a is greater than b')
+
+
+i = 4 
+j = 8
+k = 12
+
+if i>j:
+    print('i is greater than j')
+elif j>i:
+    print('j is greater than i')
+else:
+    print('if and elif')
