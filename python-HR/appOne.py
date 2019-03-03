f6b3f5915869794979c827520aeeaa12f4f3a42f
pouyapanahandeh <panahandehpooya@gmail.com>
2019-03-03 21:36:08 +0100

    checking the type of value

--- python-HR/appOne.py
+++ python-HR/appOne.py
@@ -7,6 +7,7 @@ print('Hi ' + name + ' welcome to the python3 world')
 
 birth_year = input('when is your birthday? ' )
 age = 2019 - int(birth_year) # here we need to use int() to parse birth_year from string to integer
+print(type(birth_year)) # type of birth_year, now you can undrestand why we need to parse it from strong to integer
+print(type(age))
 print(age)
 
-# 
