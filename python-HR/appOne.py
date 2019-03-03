f900e5f758cc222652e61d604ac6feaa673879fa
pouyapanahandeh <panahandehpooya@gmail.com>
2019-03-03 21:31:29 +0100

    parsing in python

--- python-HR/appOne.py
+++ python-HR/appOne.py
@@ -3,3 +3,10 @@
 name = input('what is your name? ')
 print('Hi ' + name + ' welcome to the python3 world')
 
+# calculating the age of user
+
+birth_year = input('when is your birthday? ' )
+age = 2019 - int(birth_year) # here we need to use int() to parse birth_year from string to integer
+print(age)
+
+# 
