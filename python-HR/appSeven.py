e4afe022ecfef486eec11c881aa68586f398fce5
pouyapanahandeh <panahandehpooya@gmail.com>
2019-06-02 23:15:17 +0200

    for loop in python

---
+++ python-HR/appSeven.py
@@ -0,0 +1,24 @@
+# for loop in python
+
+# here we iterate through the string we defined
+for item in 'pooya':
+	print(item)
+	
+# iterate through the array
+
+for index in ['pooya', 'amir', 'flix']:
+	print(index)
+	
+for indexer in [1,2,3,4,5,6]:
+	print(indexer)
+
+# range start from 0 to range < desire number, here is ten
+for i in range(10):
+	print(i)
+	
+# using range include start from, till desire number, and stepping,. by using range we print even number from 2 to 9
+for j in range(2, 10, 2):
+	print(j)
+	
+
+	
