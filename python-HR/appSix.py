2a8eb414fe0e424322bdda2e3f239118a782c7af
pouyapanahandeh <panahandehpooya@gmail.com>
2019-06-02 21:44:26 +0200

    make pattern by while loop

--- python-HR/appSix.py
+++ python-HR/appSix.py
@@ -7,3 +7,13 @@ while i <= counter:
 	i += 1
 print("Done")
 
+# make pattern by while loop
+
+j = 0
+star_counter = int(input("enter the end of the pattern: "))
+while j <= star_counter:
+	# here we can multiply string with number, to repeat them
+	print('*' * j)
+	j += 1
+print("Done")
+
