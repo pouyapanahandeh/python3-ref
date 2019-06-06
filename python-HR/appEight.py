bc4bce17bb6f3d47a284b09d82d548ce3eb94be5
pouyapanahandeh <panahandehpooya@gmail.com>
2019-06-07 00:03:17 +0200

    list in python

---
+++ python-HR/appEight.py
@@ -0,0 +1,37 @@
+# list in python
+
+# List is a collection which is ordered and changeable. Allows duplicate members.
+
+names = ['pooya', 'amir', 'jim', 'jeniffer']
+print(names) # print the list as we write it
+
+# index printing in array
+
+print(names[0])
+
+# print from index 1 till end
+
+print(names[1:])
+
+# print the end of the array
+
+print(names[-1])
+
+print(names) # as you can see here, those operation didn't change element of the list	
+
+# changing element of the list 
+
+names[0] = 'mohammad'	# change the first element of the array
+print(names)
+
+# find the largest number in the list
+
+number = [1,2,3,4,5,6,7]
+maxnum = number[0]
+
+# example to find the largest number in the list
+
+for i in number:
+	if i > maxnum:
+		maxnum = number[i]
+print(maxnum)
