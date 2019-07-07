199cf17508fb047db9a97f4eaad40cad4d8e5cc0
pouyapanahandeh <panahandehpooya@gmail.com>
2019-07-07 13:40:37 +0200

    list method

---
+++ python-HR/appNine.py
@@ -0,0 +1,12 @@
+# operation on list
+
+numbers = [1,2,3,4,5,6,7,8]
+numbers.append(9)	 # append use to add element to the list
+print(numbers)
+
+numbers.insert(0, 0) # by insert we can add element in specific index we want
+print(numbers)
+
+numbers.remove(8) # by remove method we can delete element from list[ it's not indexing ]
+print(numbers)
+
