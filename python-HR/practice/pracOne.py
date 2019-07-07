a9efa922c4f7bfce738601b06fb74b85663f9fad
pouyapanahandeh <panahandehpooya@gmail.com>
2019-07-07 18:14:43 +0200

    Delete the duplicate element from list

--- python-HR/practice/pracOne.py
+++ python-HR/practice/pracOne.py
@@ -1,2 +1,10 @@
 # write program to remove the duplicates in a list.
 
+numbers = [2,4,6,2,4,5,7,6,7,8,9,8,9]
+uniques = []
+
+for number in numbers:
+	if number not in uniques:
+		uniques.append(number)
+		
+print(uniques)
