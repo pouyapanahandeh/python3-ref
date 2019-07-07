d67018643dcf7e6a952b834b0739983e3e4f82f0
pouyapanahandeh <panahandehpooya@gmail.com>
2019-07-07 15:39:37 +0200

    index in list

--- python-HR/appNine.py
+++ python-HR/appNine.py
@@ -7,6 +7,12 @@ print(numbers)
 numbers.insert(0, 0) # by insert we can add element in specific index we want
 print(numbers)
 
+numbers.pop()
+print(numbers) # pop means delete the last number of the list
+
+
 numbers.remove(8) # by remove method we can delete element from list[ it's not indexing ]
 print(numbers)
+
+print(numbers.index(3)) # Shows the index of number 3
 
