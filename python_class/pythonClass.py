b8865ebb6da8065754a4df341cedfccdf3b3d2c5
pouyapanahandeh <panahandehpooya@gmail.com>
2020-11-04 20:28:44 +0100

    oop concept in py

---
+++ python_class/pythonClass.py
@@ -0,0 +1,11 @@
+class Computer:
+	
+	def Person(self):
+		print("i7, 16gb, 1TB")
+		
+
+print("---------------------")
+computer_object = Computer()
+print(type(computer_object))
+print(type(computer_object.Person()))
+computer_object.Person()
